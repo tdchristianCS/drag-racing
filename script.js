6305e85f56685b4babd7388bb654a8bc7128ac34
Daniel <daniel.kuzhel@gmail.com>
2025-05-15 09:58:04 -0400

    Added text and image

--- script.js
+++ script.js
@@ -14,9 +14,23 @@ $("#btnBackToStart").click(backToStart);
 const moveItemsIn = () => {
   $('#title').css('margin-left', 0);
   $('#btnStart').css('margin-left', 0);
-  $('#btnStartWrap').css('margin-left', 0);
-  $('btnInsight').css('margin-left', 0 );
-  $('#btnInsightWrap').css('margin-left', 0);
-}
+  $('#btnStartWrap').css('margin-left', '-15px');
+  $('btnInsight').css('margin-left', 0);
+  $('#btnInsightWrap').css('margin-left', '-15px');
+  $('#drag-strip').css('margin-right', '-210px');
+};
 
 $(document).ready(moveItemsIn);
+
+const rulescreen = () => {
+  $("ruleScreen").removeClass("hide");
+  $("#startScreen").addClass("hide");
+};
+
+const backtoStart = () => {
+  $("#rulecreen").addClass("hide");
+  $("#startScreen").removeClass("hide");
+};
+
+$("#btnInsight").click(startGame);
+$("#btnBackToStart").click(backToStart);
