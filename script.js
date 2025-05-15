3ca37b8147cd4b95ff5d7ee1e8fa9365f67ead49
Daniel <daniel.kuzhel@gmail.com>
2025-05-15 10:32:50 -0400

    Fixed your mistake (do not overwrite please)

--- script.js
+++ script.js
@@ -14,17 +14,31 @@ $("#btnBackToStart").click(backToStart);
 const moveItemsIn = () => {
   $('#title').css('margin-left', 0);
   $('#btnStart').css('margin-left', 0);
-  $('#btnStartWrap').css('margin-left', 0);
-  $('btnInsight').css('margin-left', 0 );
-  $('#btnInsightWrap').css('margin-left', 0);
-}
-
-const intiate_game = () => {
-  var stage = 0;
-  
-
+  $('#btnStartWrap').css('margin-left', '-15px');
+  $('btnInsight').css('margin-left', 0);
+  $('#btnInsightWrap').css('margin-left', '-15px');
+  $('#drag-strip').css('margin-right', '-210px');
 }
 
 $(document).ready(moveItemsIn);
+
+const rulescreen = () => {
+  $("ruleScreen").removeClass("hide");
+  $("#startScreen").addClass("hide");
+};
+
+const backtoStart = () => {
+  $("#rulecreen").addClass("hide");
+  $("#startScreen").removeClass("hide");
+};
+
+$("#btnInsight").click(startGame);
+$("#btnBackToStart").click(backToStart);
+
+const intiate_game = () => {
+  var stage = 0;
+
+
+}
 
 $('car').mouseover(intiate_game);
